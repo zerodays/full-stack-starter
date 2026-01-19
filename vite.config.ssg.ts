083d33b812dd702615e97ef2933881168be941d3
Niki Bizjak <niki.bizjak@zerodays.dev>
2026-01-19 10:13:03 +0100

    feat: disallow imports from `server` -> `web`

--- vite.config.ssg.ts
+++ vite.config.ssg.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { preventImports } from "./vite-plugins";
 
 export default defineConfig(() => {
   return {
@@ -15,6 +16,17 @@ export default defineConfig(() => {
       outDir: "dist-static",
       emptyOutDir: true,
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      // Do not allow server code to be imported into the client build
+      preventImports({
+        fromFolder: path.resolve(__dirname, 'web'),
+        folder: path.resolve(__dirname, 'server'),
+        ignores: [
+          "./server/server.tsx",
+        ]
+      }),
+      react(),
+      tailwindcss(),
+    ],
   };
 });
