c4ebca41d08d20112ddee1fdb1285579d4c93684
Tim Kalan <tim.kalan@zerodays.dev>
2026-03-16 15:04:05 +0100

    chore: fix ci

--- server/utils/testing/test-users.ts
+++ server/utils/testing/test-users.ts
@@ -4,12 +4,14 @@ export const TestUser1: InsertUser = {
   id: "00000000-0000-4000-8000-000000000001",
   email: "user1@example.com",
   name: "Test User 1",
+  createdAt: new Date(),
 };
 
 export const TestUser2: InsertUser = {
   id: "00000000-0000-4000-8000-000000000002",
   email: "user2@example.com",
   name: "Test User 2",
+  createdAt: new Date(),
 };
 
 export const testUsers = [TestUser1, TestUser2];
